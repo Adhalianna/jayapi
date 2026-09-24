028c4db080e3476304547075339653097ac533d0
Adhalianna <natka.goc@gmail.com>
2026-09-25 00:45:40 +0200

    extend `aide` feature completeness

--- jayapi/src/aide.rs
+++ jayapi/src/aide.rs
@@ -40,3 +40,76 @@ where
     }
 }
 
+impl<STATUS: DataResponseStatus, R> aide::OperationOutput for DataResponse<STATUS, Vec<R>>
+where
+    DataResponse<STATUS, Vec<R>>: schemars::JsonSchema,
+    R: schemars::JsonSchema + AsResource,
+{
+    type Inner = Self;
+
+    fn operation_response(
+        ctx: &mut aide::generate::GenContext,
+        _operation: &mut aide::openapi::Operation,
+    ) -> Option<aide::openapi::Response> {
+        use schemars::JsonSchema;
+
+        let mut response = aide::openapi::Response::default();
+        let mut content = aide::openapi::MediaType::default();
+
+        content.schema = Some(aide::openapi::SchemaObject {
+            json_schema: Self::json_schema(&mut ctx.schema),
+            external_docs: None,
+            example: None,
+        });
+
+        response
+            .content
+            .insert(String::from("application/json"), content);
+
+        Some(response)
+    }
+
+    fn inferred_responses(
+        ctx: &mut aide::generate::GenContext,
+        operation: &mut aide::openapi::Operation,
+    ) -> Vec<(Option<u16>, aide::openapi::Response)> {
+        vec![(
+            Some(STATUS::status()),
+            Self::operation_response(ctx, operation).unwrap(),
+        )]
+    }
+}
+
+#[cfg(feature = "axum")]
+impl<R: schemars::JsonSchema> aide::OperationInput for crate::extract::ResourceRequest<R> {
+    fn operation_input(
+        ctx: &mut aide::generate::GenContext,
+        operation: &mut aide::openapi::Operation,
+    ) {
+        let mut content = aide::openapi::MediaType::default();
+
+        content.schema = Some(aide::openapi::SchemaObject {
+            json_schema: R::json_schema(&mut ctx.schema),
+            external_docs: None,
+            example: None,
+        });
+
+        operation.request_body = Some(aide::openapi::ReferenceOr::Item({
+            let mut body = aide::openapi::RequestBody {
+                description: None,
+                required: true,
+                ..Default::default()
+            };
+            body.content
+                .insert(String::from("application/json"), content);
+            body
+        }))
+    }
+
+    fn inferred_early_responses(
+        _ctx: &mut aide::generate::GenContext,
+        _operation: &mut aide::openapi::Operation,
+    ) -> Vec<(Option<u16>, aide::openapi::Response)> {
+        Vec::new()
+    }
+}
--- jayapi/src/json_schema.rs
+++ jayapi/src/json_schema.rs
@@ -6,7 +6,6 @@ const JSON_SCHEMA_RESPONSE_DEF: &'static str = "jayapi_data_response";
 pub trait JsonSchema: schemars::JsonSchema {}
 impl<T> JsonSchema for T where T: schemars::JsonSchema {}
 
-
 impl<STATUS: DataResponseStatus, R: schemars::JsonSchema + AsResource> schemars::JsonSchema
     for DataResponse<STATUS, R>
 {
@@ -15,8 +14,62 @@ impl<STATUS: DataResponseStatus, R: schemars::JsonSchema + AsResource> schemars:
     }
 
     fn json_schema(generator: &mut schemars::SchemaGenerator) -> schemars::Schema {
-        attach_defs(generator);
-        todo!()
+        util::attach_resource_def(generator);
+
+        let def_path =
+            generator.settings().definitions_path.clone() + "/" + JSON_SCHEMA_RESOURCE_DEF;
+
+        schemars::json_schema!({
+            "type": "object",
+            "properties": {
+                "data": R::json_schema(generator),
+                "included": {
+                    "type": "array",
+                    "items": {
+                        "$ref": def_path,
+                    }
+                },
+                "links": {
+                    "type": "object"
+                }
+            },
+            "required": ["data"]
+        })
+    }
+}
+
+impl<STATUS: DataResponseStatus, R: schemars::JsonSchema + AsResource> schemars::JsonSchema
+    for DataResponse<STATUS, Vec<R>>
+{
+    fn schema_name() -> std::borrow::Cow<'static, str> {
+        std::borrow::Cow::Borrowed(R::ty())
+    }
+
+    fn json_schema(generator: &mut schemars::SchemaGenerator) -> schemars::Schema {
+        util::attach_resource_def(generator);
+
+        let def_path =
+            generator.settings().definitions_path.clone() + "/" + JSON_SCHEMA_RESOURCE_DEF;
+
+        schemars::json_schema!({
+            "type": "object",
+            "properties": {
+                "data": {
+                    "type": "array",
+                    "items": R::json_schema(generator)
+                },
+                "included": {
+                    "type": "array",
+                    "items": {
+                        "$ref": def_path,
+                    }
+                },
+                "links": {
+                    "type": "object"
+                }
+            },
+            "required": ["data"]
+        })
     }
 }
 
@@ -26,7 +79,7 @@ impl<STATUS: DataResponseStatus> schemars::JsonSchema for DataResponse<STATUS, (
     }
 
     fn json_schema(generator: &mut schemars::SchemaGenerator) -> schemars::Schema {
-        attach_resource_def(generator);
+        util::attach_resource_def(generator);
 
         let def_path =
             generator.settings().definitions_path.clone() + "/" + JSON_SCHEMA_RESOURCE_DEF;
@@ -53,89 +106,94 @@ impl<STATUS: DataResponseStatus> schemars::JsonSchema for DataResponse<STATUS, (
                         "$ref": def_path,
                     }
                 },
+                "links": {
+                    "type": "object"
+                }
             },
             "required": ["data"],
         })
     }
 }
-fn attach_resource_def(generator: &mut schemars::SchemaGenerator) {
-    let defs = generator.definitions_mut();
-    match defs.entry(JSON_SCHEMA_RESOURCE_DEF) {
-        serde_json::map::Entry::Vacant(vacant_entry) => {
-            vacant_entry.insert(
-                schemars::json_schema!({
-                    "type": "object",
-                    "properties": {
-                        "type": {
-                            "type": "string"
-                        },
-                        "id": {
-                            "type": "string"
-                        },
-                        "attributes": {
-                            "type": "object"
-                        },
-                        "relationships": {
-                            "type": "object"
-                        },
-                        "links": {
-                            "type": "object"
-                        },
-                    },
-                    "required": ["type", "id"],
-                    "additionalProperties": false,
-                })
-                .to_value()
-                .to_owned(),
-            );
-        }
-        serde_json::map::Entry::Occupied(_) => {}
-    };
-}
 
-fn attach_response_def(generator: &mut schemars::SchemaGenerator) {
-    let def_path = generator.settings().definitions_path.clone() + "/" + JSON_SCHEMA_RESOURCE_DEF;
-    let defs = generator.definitions_mut();
-    match defs.entry(JSON_SCHEMA_RESPONSE_DEF) {
-        serde_json::map::Entry::Vacant(vacant_entry) => {
-            vacant_entry.insert(
-                schemars::json_schema!({
-                    "type": "object",
-                    "properties": {
-                        "data": {
-                            "oneOf": [
-                                {
-                                    "$ref": def_path
-                                },
-                                {
-                                    "type": "array",
-                                    "items": {
+pub mod util {
+    pub fn attach_resource_def(generator: &mut schemars::SchemaGenerator) {
+        let defs = generator.definitions_mut();
+        match defs.entry(super::JSON_SCHEMA_RESOURCE_DEF) {
+            serde_json::map::Entry::Vacant(vacant_entry) => {
+                vacant_entry.insert(
+                    schemars::json_schema!({
+                        "type": "object",
+                        "properties": {
+                            "type": {
+                                "type": "string"
+                            },
+                            "id": {
+                                "type": "string"
+                            },
+                            "attributes": {
+                                "type": "object"
+                            },
+                            "relationships": {
+                                "type": "object"
+                            },
+                            "links": {
+                                "type": "object"
+                            },
+                        },
+                        "required": ["type", "id"],
+                        "additionalProperties": false,
+                    })
+                    .to_value()
+                    .to_owned(),
+                );
+            }
+            serde_json::map::Entry::Occupied(_) => {}
+        };
+    }
+
+    pub fn attach_response_def(generator: &mut schemars::SchemaGenerator) {
+        let def_path =
+            generator.settings().definitions_path.clone() + "/" + super::JSON_SCHEMA_RESOURCE_DEF;
+        let defs = generator.definitions_mut();
+        match defs.entry(super::JSON_SCHEMA_RESPONSE_DEF) {
+            serde_json::map::Entry::Vacant(vacant_entry) => {
+                vacant_entry.insert(
+                    schemars::json_schema!({
+                        "type": "object",
+                        "properties": {
+                            "data": {
+                                "oneOf": [
+                                    {
                                         "$ref": def_path
                                     },
-                                    "uniqueItems": true,
+                                    {
+                                        "type": "array",
+                                        "items": {
+                                            "$ref": def_path
+                                        },
+                                        "uniqueItems": true,
+                                    }
+                                ]
+                            },
+                            "included": {
+                                "type": "array",
+                                "items": {
+                                    "$ref": def_path,
                                 }
-                            ]
+                            },
                         },
-                        "included": {
-                            "type": "array",
-                            "items": {
-                                "$ref": def_path,
-                            }
-                        },
-                    },
-                    "required": ["data"],
-                })
-                .to_value()
-                .to_owned(),
-            );
-        }
-        serde_json::map::Entry::Occupied(_) => {}
-    };
+                        "required": ["data"],
+                    })
+                    .to_value()
+                    .to_owned(),
+                );
+            }
+            serde_json::map::Entry::Occupied(_) => {}
+        };
+    }
+
+    pub fn attach_defs(generator: &mut schemars::SchemaGenerator) {
+        attach_resource_def(generator);
+        attach_response_def(generator);
+    }
 }
-
-fn attach_defs(generator: &mut schemars::SchemaGenerator) {
-    attach_resource_def(generator);
-    attach_response_def(generator);
-}
-
-
